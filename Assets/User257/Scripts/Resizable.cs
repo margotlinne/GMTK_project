de7fcf9e9db50abba99a26ace6fb67b5d9cc6ff8
Eo jin <ddw4500@naver.com>
2024-08-18 11:30:22 +0900

    feat: resizable object

---
+++ Assets/User257/Scripts/Resizable.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resizable : MonoBehaviour
+{
+    public bool doResize;
+    float wheelAmount;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log($"resizable object {gameObject.name} is clicked");
+                doResize = true;
+            }
+        }
+
+        OnResize();
+    }
+
+    void OnResize()
+    {
+        if (!doResize)
+            return;
+
+        if (wheelAmount > 0)
+            Debug.Log($"make {gameObject.name} bigger");
+        else if (wheelAmount < 0)
+            Debug.Log($"make {gameObject.name} smaller");
+
+        wheelAmount = Input.GetAxis("Mouse ScrollWheel");
+
+        transform.localScale = transform.localScale * (wheelAmount + 1f);
+    }
+}
