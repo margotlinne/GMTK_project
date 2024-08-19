e263d06a4e014f845804314f4ce07854c1273e53
choi-m-09 <audrbchzl@gmail.com>
2024-08-20 07:55:37 +0900

    fear: Object resize and Work on animation

---
+++ Assets/Choi/Script/St1_GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Choi
+{
+    public class St1_GameManager : MonoBehaviour
+    {
+        [SerializeField]
+        BoxCollider2D[] walls;
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            
+        }
+
+        
+    }
+}
