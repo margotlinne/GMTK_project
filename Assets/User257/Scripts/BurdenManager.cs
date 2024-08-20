0ba39ee5d6f138ac08ecd8d227f1e83e5ada8256
Eo jin <ddw4500@naver.com>
2024-08-20 20:24:31 +0900

    feat: change face when burden disappear

--- Assets/User257/Scripts/BurdenManager.cs
+++ Assets/User257/Scripts/BurdenManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,9 +11,11 @@ namespace User257
     {
         [SerializeField] List<Burden> burdens = new List<Burden>();
 
-        int burdenAmount;
+        [SerializeField] GameObject face_bad;
+        [SerializeField] GameObject face_soso;
+        [SerializeField] GameObject face_normal;
 
-        [SerializeField] TMP_Text clear;
+        int burdenAmount;
 
         private void Awake()
         {
@@ -20,22 +23,48 @@ namespace User257
 
             foreach (Burden element in burdens)
                 element.OnDisappear += Discount;
+        }
 
-            clear.gameObject.SetActive(false);
+        private void Start()
+        {
+            DisactiveFace();
+            face_bad.SetActive(true);
         }
 
         void Discount()
         {
             burdenAmount--;
 
+            DisactiveFace();
+
+            switch (burdenAmount)
+            {
+                case 3:
+                    face_bad.SetActive(true);
+                    break;
+                case 2:
+                case 1:
+                    face_soso.SetActive(true);
+                    break;
+                case 0:
+                    face_normal.SetActive(true);
+                    break;
+            }
+
             if (burdenAmount <= 0)
                 GameClear();
-
         }
 
         public void GameClear()
         {
             SceneManager.LoadScene("Stage_3_3");
+        }
+
+        void DisactiveFace()
+        {
+            face_bad.SetActive(false);
+            face_soso.SetActive(false);
+            face_normal.SetActive(false);
         }
     }
 }
