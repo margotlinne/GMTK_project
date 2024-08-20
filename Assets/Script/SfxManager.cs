a1280b316333cc82ad22c5845412622e446cce09
itsparacha <shahzaibparacha95@gmail.com>
2024-08-20 05:01:32 -0700

    sfx manager

--- Assets/Script/SfxManager.cs
+++ Assets/Script/SfxManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SfxManager : MonoBehaviour
 {
@@ -13,6 +15,20 @@ public class SfxManager : MonoBehaviour
      * 
      * 
      */
+
+    public AudioClip footStepSoundClip;
+
+    public AudioClip ClassRoomAmbSoundClip;
+
+    public AudioClip ClassAmbNegativeSoundClip;
+
+    public AudioClip BreatheSoundClip;
+
+    public AudioClip ChildrenPlayingSoundClip;
+
+    public AudioClip InteractionAppearSoundClip;
+
+    public AudioClip InteractionDisappearSoundClip;
 
     void Awake()
     {
@@ -27,6 +43,21 @@ public class SfxManager : MonoBehaviour
          * kidWalkingSoundClip = Resources.Load<AudioClip>("kidwalking");
          
          */
+
+        footStepSoundClip = Resources.Load<AudioClip>("Sfx_Ftstp_01");
+
+        ClassRoomAmbSoundClip = Resources.Load<AudioClip>("Amb_Classroom");
+
+        ClassAmbNegativeSoundClip = Resources.Load<AudioClip>("Amb_Classroom_Negative");
+
+        BreatheSoundClip = Resources.Load<AudioClip>("Dx_Breathing");
+
+        ChildrenPlayingSoundClip = Resources.Load<AudioClip>("Amb_ChildernPlaying");
+
+        InteractionAppearSoundClip = Resources.Load<AudioClip>("Sfx_Interaction_Appear");
+
+        InteractionDisappearSoundClip = Resources.Load<AudioClip>("Sfx_Interaction_Disappear");
+
     }
 
     /*
@@ -38,6 +69,41 @@ public class SfxManager : MonoBehaviour
      * }
      * 
      */
+
+    public void footStepSoundPlay()
+    {
+        audioSource.PlayOneShot(footStepSoundClip);
+    }
+
+    public void ClassRoomAmbSoundPlay()
+    {
+        audioSource.PlayOneShot(ClassRoomAmbSoundClip);
+    }
+
+    public void ClassAmbNegative()
+    {
+        audioSource.PlayOneShot(ClassAmbNegativeSoundClip);
+    }
+
+    public void BreatheSoundPlay() 
+    {
+        audioSource.PlayOneShot(BreatheSoundClip);
+    }
+
+    public void ChildrenPlayingSoundPlay()
+    {
+        audioSource.PlayOneShot(ChildrenPlayingSoundClip);
+    }
+
+    public void InteractionAppearSoundPlay()
+    {
+        audioSource.PlayOneShot(InteractionAppearSoundClip);
+    }
+
+    public void InteractionDisappearSoundPlay()
+    {
+        audioSource.PlayOneShot(InteractionDisappearSoundClip);
+    }
 
 
 }
