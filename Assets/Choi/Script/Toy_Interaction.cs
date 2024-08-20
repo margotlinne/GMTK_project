9e6076cba3b74ef45c760a64fa8548f5d2fc0bb9
Eo jin <ddw4500@naver.com>
2024-08-21 01:16:24 +0900

    import choi

--- Assets/Choi/Script/Toy_Interaction.cs
+++ Assets/Choi/Script/Toy_Interaction.cs
@@ -7,6 +7,7 @@ namespace Choi
     public class Toy_Interaction : MonoBehaviour
     {
         public GameObject shelf_Toy;
+        public GameObject interaction_Key;
         public bool activate = false;
         Transform handPos;
 
@@ -15,28 +16,38 @@ namespace Choi
         void Start()
         {
             handPos = GetComponentInParent<Toy_Manager>().player_Hand;
+            interaction_Key.SetActive(false);
         }
 
         void OnTriggerEnter2D(Collider2D collision)
         {
-            if (activate && !holding && !handPos.GetComponentInParent<PlayerController_st1>().hold) GetComponent<SpriteOutline>().outlineSize = 1;
+            if (activate && !holding && !handPos.GetComponentInParent<PlayerController_st1>().hold)
+            {
+                GetComponent<SpriteOutline>().outlineSize = 1;
+                interaction_Key.SetActive(true);
+            }
             else return;
         }
 
         void OnTriggerStay2D(Collider2D collision)
         {
-            if(activate && handPos.childCount == 0 && !holding && Input.GetKeyDown(KeyCode.E))
+            if(activate && handPos.childCount == 0 && !holding)
             {
-                holding = true;
-                transform.position = handPos.position;
-                transform.parent = null;
-                transform.parent = handPos;
+                if (Input.GetKey(KeyCode.E))
+                {
+                    holding = true;
+                    transform.position = handPos.position;
+                    transform.parent = null;
+                    transform.parent = handPos;
+                    Destroy(interaction_Key);
+                }
             }
         }
 
         void OnTriggerExit2D(Collider2D collision)
         {
             GetComponent<SpriteOutline>().outlineSize = 0;
+            if(interaction_Key != null) interaction_Key.SetActive(false);
         }
     }
 }
--- Assets/Choi/Script/WardrobeMonster.cs
+++ Assets/Choi/Script/WardrobeMonster.cs
@@ -7,6 +7,7 @@ namespace Choi
 {
     public class WardrobeMonster : MonoBehaviour
     {
+        public GameObject interaction;
         [SerializeField]
         GameObject Wall;
 
@@ -17,16 +18,25 @@ namespace Choi
             map_light = GameObject.Find("Maps").GetComponent<Light2D>();
         }
 
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            interaction.SetActive(true);
+        }
+
         private void OnTriggerStay2D(Collider2D collision)
         {
             // 상호작용 UI 표시
             if (Input.GetKeyDown(KeyCode.E))
             {
                 Destroy(Wall);
-                // 상호작용 UI 페이드 아웃
-                Destroy(this.gameObject);
                 map_light.intensity += 0.2f;
+                Destroy(this.gameObject);
             }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            interaction.SetActive(false);
         }
     }
 }
